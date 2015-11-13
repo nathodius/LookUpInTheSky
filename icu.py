0f54b5d5d3c8fa7ba5286e75165b6c268f38e0d7
Nathaniel Hughes <nathanielhughes@Nathaniels-MacBook-Pro-2.local>
2015-11-13 15:58:38 -0500

    Convert zip code to long-lat.

--- icu.py
+++ icu.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 
 import getopt, sys
+import geocoder
+
 
 def main(argv):
 
-	print("running...")
 
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "z:s:")
-		print ("init getopt")
 	except getopt.GetoptError as err:
-		print ("uhoh")
 		print str(err) # will print something like "option -a not recognized"
-		sys.exit(2)
-
-	print("done with try-except")
-
+		sys.exit(2) 
 
 	zipCode = None
-	NORAD_CatalogNumber = None  
-
-	print ("init vars") 
+	NORAD_CatalogNumber = None
 
 	print("zip code", zipCode)
 	print("NORAD catalog number", NORAD_CatalogNumber)	
@@ -33,6 +27,11 @@ def main(argv):
 
 	print("zip code", zipCode)
 	print("NORAD catalog number", NORAD_CatalogNumber)
+
+	if zipCode != None:
+		g = geocoder.google(zipCode)
+		coordinates = g.latlng
+		print("coordinates", coordinates)
 
 if __name__ == "__main__":
     main(sys.argv)
