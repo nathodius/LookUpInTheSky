d2b210dab7f484ed1f2441084738c14bfa179a06
Christina Nguyen <phuonghien716@gmail.com>
2015-11-13 20:00:26 -0500

    Uselessness

--- icu.py
+++ icu.py
@@ -4,7 +4,7 @@ import getopt, sys
 import geocoder
 import  pywapi
 import string
-
+import ephem
 
 def main(argv):
 
@@ -38,6 +38,26 @@ def main(argv):
 	weather_com_result = pywapi.get_weather_from_weather_com(zipCode)
 	current_conditions = weather_com_result['current_conditions']['text']
 	print ("current conditions", current_conditions)
+
+	##############################################################################
+#	satellite = "VANGUARD 3"
+#	line1 = "1 00020U 59007A   15315.53193246  .00000593  00000-0  24329-3 0  9991"
+#	line2 = "2 00020  33.3468  44.5961 1669346 358.5309   1.0765 11.55084888 36273"
+	
+#	lat = str(g.lat)
+#	lng = str(g.lng)
+#	print(lat)
+#	print(lng)
+
+	# Read TLE
+#	tle = ephem.readtle(satellite, line1, line2)
+
+	# Compute TLE after setting the datetime
+#	observer = ephem.Observer()
+#	observer.lat = str(g.lat)
+#	observer.long = str(g.lng)
+	#tle.compute(ephem.Observer())
+	##############################################################################
 
 
 if __name__ == "__main__":
